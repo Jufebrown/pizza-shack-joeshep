9f70f246e7be25d8c4af362f840c6163b0afac8d
Jufebrown <jufebrown@gmail.com>
2017-03-29 14:07:57 -0500

    started app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+`use strict`
+
+const express = require('express')
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
