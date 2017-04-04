02b5c077dc380bfaa7eacefe5b2bb61438f98aaa
Jufebrown <jufebrown@gmail.com>
2017-04-04 15:15:47 -0500

    installed knex, bookshelf and postgres

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const router = Router()
 
 // public routes
 router.use(require(`./about`))
-router.use(require(`./contact`))
-router.use(require(`./login`))
-router.use(require(`./register`))
+// router.use(require(`./contact`))
+// router.use(require(`./login`))
+// router.use(require(`./register`))
 router.use(require(`./root`))
 
 
@@ -22,7 +22,7 @@ router.use((req, res, next) => {
 })
 
 // private routes
-router.use(require('/logout'))
-router.use(require('/order'))
+// router.use(require('/logout'))
+// router.use(require('/order'))
 
 module.exports = router
