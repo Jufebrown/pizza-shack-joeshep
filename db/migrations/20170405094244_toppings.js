2082e726e99c77cb62a7c3c2f37caed8d90db733
Jufebrown <jufebrown@gmail.com>
2017-04-05 09:45:43 -0500

    added schema for toppings table

--- db/migrations/20170405094244_toppings.js
+++ db/migrations/20170405094244_toppings.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('toppings', (table) => {
+      table.increments()
+      table.string('toppings_name').notNullable().unique()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('toppings')
 };
