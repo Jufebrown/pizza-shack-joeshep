f168cf13a683e56315832d7027d3a58bb5e760bd
Jufebrown <jufebrown@gmail.com>
2017-04-05 09:39:10 -0500

    added schema for orders table

--- db/migrations/20170405093421_orders.js
+++ db/migrations/20170405093421_orders.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('orders', (table) => {
+      table.increments()
+      table.string('order_name').notNullable()
+      table.string('order_email').notNullable()
+      table.string('order_phone').notNullable()
+      table.integer('order_size_id').notNullable()
+      table.specificType('order_toppings', knex.raw('text[]')).notNullable().defaultTo('{"cheese"}')
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTable('orders')
 };
