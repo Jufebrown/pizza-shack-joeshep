41c41ec8dad2897c63dbd30f69c81fbe0bce6517
Jufebrown <jufebrown@gmail.com>
2017-04-05 09:33:57 -0500

    added schema for contacts

--- db/migrations/20170404154321_initial.js
+++ db/migrations/20170404154321_initial.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema
+    .createTable('contacts', (table) => {
+      table.increments()
+      table.string('name').notNullable().unique()
+      table.string('email').notNullable()
+      table.string('phone').notNullable()
+      table.string('message').notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema
+    .dropTableIfExists('contacts')
 };
