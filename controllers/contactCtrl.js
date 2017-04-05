e7ab9a69345028b9f2d519caf25c51a7179e92d6
Jufebrown <jufebrown@gmail.com>
2017-04-05 11:47:51 -0500

    added addContact method to contactCtrl

--- controllers/contactCtrl.js
+++ controllers/contactCtrl.js
@@ -1,5 +1,16 @@
 `use strict`
 
+const Contact = require('../models/contactModel')
+
 module.exports.show = (req, res, next) => {
   res.render('contact', {page: 'Contact'})
+}
+
+module.exports.addContact = ({body}, res, err) => {
+  Contact.forge(body)
+  .save()
+  .then(() => {
+    res.redirect('/')
+  })
+  .catch(err)
 }
