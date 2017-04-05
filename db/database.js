6ae320a2507c5d307344a5e8d4c0d526e544faae
Jufebrown <jufebrown@gmail.com>
2017-04-05 10:14:37 -0500

    added database.js config file

---
+++ db/database.js
@@ -0,0 +1,3 @@
+`use strict`
+
+const environment = process.env.NODE_ENV || 'development'
--- db/seeds/toppings_seed.js
+++ db/seeds/toppings_seed.js
@@ -1,10 +1,12 @@
+`use strict`
+const {knex} = require('knex')
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('toppings').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('toppings').insert([
         {id: 1, colName: 'rowValue1'},
         {id: 2, colName: 'rowValue2'},
         {id: 3, colName: 'rowValue3'}
