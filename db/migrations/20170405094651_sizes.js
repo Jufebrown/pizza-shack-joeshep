4533b25f74e65ad586cc281b14c3db9d38915c0c
Jufebrown <jufebrown@gmail.com>
2017-04-05 09:49:43 -0500

    added schema for sizes table

--- db/migrations/20170405094651_sizes.js
+++ db/migrations/20170405094651_sizes.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema
+    .createTable('sizes', (table) => {
+      table.increments()
+      table.string('sizes_name').notNullable().unique()
+      table.string('sizes_inches').notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('sizes')
 };
