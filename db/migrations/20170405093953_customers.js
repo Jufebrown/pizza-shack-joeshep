6e941162be3e5d791e22364dc432c69a5b8ab4ba
Jufebrown <jufebrown@gmail.com>
2017-04-05 09:42:16 -0500

    added schema for customers table

--- db/migrations/20170405093953_customers.js
+++ db/migrations/20170405093953_customers.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema
+    .createTable('customers', (table) => {
+      table.increments()
+      table.string('order_email').notNullable()
+      table.string('order_password').notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTable('customers')
 };
